fdaac95d2c764309a3cc998abefb7e07187021b8
azamdev105 <azammuhammad.1001@outlook.com>
2026-03-10 00:59:33 +0800

    Structure and style menu section

---
+++ script.js
@@ -0,0 +1,30 @@
+const sidebar = document.querySelector('.sidebar');
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('#close');
+const sidebarLinks = document.querySelectorAll('.sidebar a');
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.appendChild(overlay);
+
+hamburger.addEventListener('click', () => {
+    sidebar.classList.add('open');
+    overlay.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    overlay.style.display = 'none';
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    overlay.style.display = 'none';
+});
+
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sidebar.classList.remove('open');
+        overlay.style.display = 'none';
+    });
+});
